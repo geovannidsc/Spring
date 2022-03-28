be2f4b6dd2fb1b2e25b7facf733824d40a49ae05
Geovanni Correia <geovanni-dsc@hotmail.com>
2022-03-27 21:30:29 -0300

    Classe de Dominio

---
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -0,0 +1,37 @@
+package com.example.demo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
+
+@RestController
+//@EnableAutoConfiguration
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	//@RequestMapping("/")
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escrotorio");
+
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+	
+	
+
+	
+	
+}
